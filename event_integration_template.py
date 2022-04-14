e7a6a47d2a7b360d6de0c1e4160ca34da44063c9
pvz01 <81989793+pvz01@users.noreply.github.com>
2022-04-14 16:43:41 -0500

    Update event_integration_template.py

--- event_integration_template.py
+++ event_integration_template.py
@@ -1,20 +1,47 @@
-import deepinstinct30 as di
+# Import required libraries
+import requests, time, json
 
-#server configuration
-di.fqdn = 'FOO.customers.deepinstinctweb.com'
-di.key = 'BAR'
 
-#runtime
-last_id = 0
+# Define server configuration
+fqdn = 'FOO.customers.deepinstinctweb.com'
+key = 'BAR'
+
+
+# Runtime
+
+highest_event_id_collected_previously = 0
+
 while True:
-    events = di.get_events(minimum_event_id=last_id)
-    for event in events:
 
-        #TODO: Insert code here to ingest data in SIEM, generate e-mail, or take
-        #      other desired action. For now the placeholder just prints event
-        #      to console.
-        print(event, '\n')
+    last_id = highest_event_id_collected_previously
 
-        if event['id'] > last_id:
-            last_id = event['id']
-    time.sleep(300) #wait 5 minutes, then query server again for new events
+    while last_id != None:
+
+        request_url = f'https://{fqdn}/api/v1/events/?after_event_id={last_id}'
+        response = requests.get(request_url, headers={'accept': 'application/json', 'Authorization': key})
+
+        if response.status_code == 200:
+
+            if 'last_id' in response.json():
+                last_id = response.json()['last_id']
+            else:
+                last_id = None
+
+            if 'events' in response.json():
+                events = response.json()['events']
+                print('INFO: Found', len(events), 'events on query to', request_url)
+                for event in events:
+                    if event['id'] > highest_event_id_collected_previously:
+                        highest_event_id_collected_previously = event['id']
+
+                    #TODO: Insert code here to ingest data in SIEM, generate e-mail, or take
+                    #      other desired action. For now the placeholder pretty-prints event
+                    #      to console.
+                    print(json.dumps(event, indent=4), '\n')
+
+        else:
+            print('ERROR: Unexpected return code', response.status_code, 'on GET', request_url)
+
+
+    print('INFO: Sleeping for 5 minutes')
+    time.sleep(300)
