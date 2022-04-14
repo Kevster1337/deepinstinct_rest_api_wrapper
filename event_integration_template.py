8f4543059249bc144fa4286863a3e06c115e69a8
pvz01 <81989793+pvz01@users.noreply.github.com>
2022-04-14 10:59:49 -0500

    Update event_integration_template.py

--- event_integration_template.py
+++ event_integration_template.py
@@ -1,16 +1,25 @@
-#import required libraries
-import deepinstinct30 as di, time
+import deepinstinct30 as di
 
 #server configuration
 di.fqdn = 'FOO.customers.deepinstinctweb.com'
 di.key = 'BAR'
 
 #runtime
+
 last_id = 0
+
 while True:
+
     events = di.get_events(minimum_event_id=last_id)
+
     for event in events:
-        print(event, '\n') #TODO: Replace with code to forward to SIEM
+
+        #TODO: Insert code here to ingest data in SIEM, generate e-mail, or take
+        #      other desired action. For now the placeholder just prints event
+        #      to console.
+        print(event, '\n')
+
         if event['id'] > last_id:
             last_id = event['id']
-    time.sleep(300) #wait 5 minutes before querying for new events
+
+    time.sleep(300) #wait 5 minutes, then query server again for new events
